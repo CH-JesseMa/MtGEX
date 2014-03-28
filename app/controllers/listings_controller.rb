3aac3418d8db7469ab6c903152c218680c7668ac
CH-JesseMa <jesse@casthaven.com>
2014-03-28 14:47:18 -0400

    added create to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,18 @@
 class ListingsController < ApplicationController
 
   def new
-
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    @listing.save
+    redirect_to @listing
+  end
+
+private
+
+  def listing_params
+    params.require(:listing).permit(:card, :edition, :type, :condition, :quantity, :price, :description, :image_front, :image_back)
   end
 
 end
