20a2767edb206bef34f45dc0333b1b488143fc4b
CH-JesseMa <jesse@casthaven.com>
2014-03-28 11:55:15 -0400

    add new rails app mtgex with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mtgex::Application.config.secret_key_base = 'd6346fc8f0fe6e7cf733b65ac3cf2fe9d86e2725e0ee1efc213414b6d8797c69199709f01c6959fc61c564b8ba8a1e7f70fdedcf01e9ac5caea3aa135b41e72e'
