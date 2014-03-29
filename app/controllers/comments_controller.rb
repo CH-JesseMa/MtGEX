b3a52ca9c18985c7aa3cf800ebb79729f483d084
CH-JesseMa <jesse@casthaven.com>
2014-03-29 16:01:18 -0400

    add comments using partial form and partial collection

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @comment = @listing.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to listing_path(@listing)
+  end
+
+  def delete
+    @listing = Listing.find(params[:listing_id])
+    @comment = @listing.comments.find(params[:id])
+    @comment.destroy
+    redirect_to listing_path(@listing)
+  end
 end
