bda88e2c05f450b4768f2c3f815fae578b0459ed
CH-JesseMa <jesse@casthaven.com>
2014-03-29 15:45:13 -0400

    add comment resources to routes as nested in listings

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Mtgex::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  resources :listings
+
+  resources :listings do
+    resources :comments
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
