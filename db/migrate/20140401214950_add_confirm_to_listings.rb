e4a1a40300208b49e80277e8f70af9ceff6c4dd5
CH-JesseMa <jesse@casthaven.com>
2014-04-01 18:30:23 -0400

    delete columns

--- db/migrate/20140401214950_add_confirm_to_listings.rb
+++
@@ -1,8 +0,0 @@
-class AddConfirmToListings < ActiveRecord::Migration
-  def change
-    add_column :listings, :buyer_amount, :integer
-    add_column :listings, :seller_amount, :integer
-    add_column :listings, :total_amount, :integer
-
-  end
-end
