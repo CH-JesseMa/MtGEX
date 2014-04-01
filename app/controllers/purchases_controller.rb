dd1c467b5bc69dcc678c4ef79d71fdbf5f1397df
CH-JesseMa <jesse@casthaven.com>
2014-04-01 12:03:16 -0400

    listing user can't purchase card

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,11 +5,22 @@ class PurchasesController < ApplicationController
   end
 
   def create
-
+    @listing = Listing.find_by(id: purchase_params[:listing_id] )
+    @purchase = Purchase.new(purchase_params)
+    @purchase.save
+    redirect_to listing_path(@listing)
   end
 
   def show
 
   end
 
+private
+
+  def purchase_params
+    params.require(:purchase).permit(
+      :id,
+      :user_id,
+      :listing_id)
+  end
 end
