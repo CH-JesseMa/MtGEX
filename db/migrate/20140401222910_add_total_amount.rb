da4b320ba3a906449d0cd8981c9c5702b74cc01b
CH-JesseMa <jesse@casthaven.com>
2014-04-02 10:59:51 -0400

    remove migrationss(old)

--- db/migrate/20140401222910_add_total_amount.rb
+++
@@ -1,5 +0,0 @@
-class AddTotalAmount < ActiveRecord::Migration
-  def change
-    add_column :listings, :total_amount, :integer
-  end
-end
--- db/migrate/20140402012603_create_charges.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharges < ActiveRecord::Migration
-  def change
-    create_table :charges do |t|
-      t.integer :listing_id
-
-      t.timestamps
-    end
-  end
-end
