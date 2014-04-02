95b31b5ebb14c2bd0eef4095163390c97c3aff52
CH-JesseMa <jesse@casthaven.com>
2014-04-02 11:05:13 -0400

    remove seed reg users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user_admin = User.create({ username: "Admin", email: "Admin", password: "password", password_confirmation: "password", admin: true })
-buyer = User.create({ username: "Buyer", email: "Buyer", password: "Buyer", password_confirmation: "Buyer", admin: false })
-seller = User.create({ username: "Seller", email: "Seller", password: "Seller", password_confirmation: "Seller", admin: false })
